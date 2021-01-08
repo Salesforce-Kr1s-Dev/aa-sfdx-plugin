0efc280058bf87fd8cdba4aebce12bb5ef27fdb0
asdfgyu <1kn0wYo0u@gmail.com>
2021-01-08 14:30:21 +0800

    remove throw error

--- src/shared/exec.ts
+++ src/shared/exec.ts
@@ -3,15 +3,14 @@ import * as util from 'util';
 
 const execCom = util.promisify(exec);
 
-const exec2SON = async (cmd: string, options = {maxBuffer: 1024 * 1024 * 10}): Promise<any> => {
-    let message;
-    await execCom(cmd, options)
-    .then(response => {
-        message = response.stdout ? response.stdout : response.stderr;
-    })
-    .catch(err => {
-        throw new Error(err.stderr || err.stdout);
-    })
+const exec2SON = async (cmd: string, options = { maxBuffer: 1024 * 1024 * 10 }): Promise<any> => {
+    const message = await execCom(cmd, options)
+        .then(response => {
+            return JSON.parse(response.stdout ? response.stdout : response.stderr);
+        })
+        .catch(err => {
+            return JSON.parse(err.stdout ? err.stdout : err.stderr);
+        })
     return message;
 }
 
